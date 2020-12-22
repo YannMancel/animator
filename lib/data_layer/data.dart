ac1563af4690005b7e5f72f6d9f058dfc6c716d9
Yann Mancel <yann.mancel@hotmail.fr>
2020-12-22 09:50:30 +0100

    Add fade_in_image.dart

--- lib/data_layer/data.dart
+++ lib/data_layer/data.dart
@@ -1,15 +1,19 @@
 import 'package:animator/data_layer/section.dart';
 import 'package:animator/ui_layer/animations/animated_container.dart';
+import 'package:animator/ui_layer/animations/fade_in_image.dart';
 import 'package:flutter/material.dart';
 
 // -- Animations --
 
-final animations = <Section> [
+final animations = <Section>[
   Section(
       title: "AnimatedContainer",
       icon: Icons.score,
       destination: AnimatedContainerWidget()),
-  Section(title: "test", icon: Icons.title, destination: Container(color: Colors.blue)),
+  Section(
+      title: "FadeInImage",
+      icon: Icons.local_movies,
+      destination: FadeInImageWidget()),
   Section(title: "bla", icon: Icons.settings, destination: Container(color: Colors.green))
 ];
 
---
+++ lib/ui_layer/animations/fade_in_image.dart
@@ -0,0 +1,26 @@
+import 'package:animator/data_layer/data.dart';
+import 'package:flutter/material.dart';
+
+/// A [StatelessWidget] subclass.
+class FadeInImageWidget extends StatelessWidget {
+
+  // FIELDS --------------------------------------------------------------------
+
+  final _url = "https://avatars0.githubusercontent.com/u/44132125?v=4";
+
+  // METHODS -------------------------------------------------------------------
+
+  // -- StatelessWidget --
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: FadeInImage(
+            placeholder: AssetImage(imageFlutter),
+            image: NetworkImage(_url),
+            fadeInCurve: Curves.linear,
+            fadeOutCurve: Curves.linear,
+            fadeInDuration: Duration(seconds: 1),
+            fadeOutDuration: Duration(seconds: 1)));
+  }
+}
